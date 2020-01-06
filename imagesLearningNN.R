4ce9f54c7117dfb5277421108d21da1ddb9dd5f4
Bolun CAI <vegeballoon@gmail.com>
2020-01-06 13:39:12 +0100

    Added CNN model, but problem with the dimension of x_train

--- imagesLearningNN.R
+++ imagesLearningNN.R
@@ -4,6 +4,7 @@ library(imager)
 
 install_keras()
 
+## Sequential model
 # Functions
 processImage = function(img) {
   # Convert to grey
@@ -22,16 +23,6 @@ processImage = function(img) {
 nb_cats = 590
 nb_cars = 485
 nb_flowers = 521
-
-## Play
-path = file.path(getwd(), 'images_train/cat/cat_train_1.jpg')
-img = readImage(path)
-imageShow(img)
-img = rgb_2gray(img)
-img = resizeImage(img, width = 100, height = 100)
-length(img)
-img = processImage(img)
-## EndPlay
 
 data = matrix(ncol = 10001, nrow = nb_cats + nb_cars + nb_flowers)
 
@@ -65,41 +56,18 @@ for (i in 1:nb_flowers) {
   data[i + nb_cars + nb_cats,] = img
 }
 
-# scale
-# s <- apply(data,1,sd)
-# ii <- which(s>1)
-# data <- data[ii,]
-
-
-# The mnist dataset is a set of images 28x28 of handwritten numbers.
-# mnist <- dataset_mnist()
-# x_train <- mnist$train$x
-# y_train <- mnist$train$y
-# x_test <- mnist$test$x
-# y_test <- mnist$test$y
-
 x_train <- data[, -10001]
 y_train <- data[, 10001]
 y_train <- to_categorical(y_train, 3)
-
-# The x data is a 3-d array (images,width,height) of grayscale values.
-
-# reshape
-# dim(data) <- c(nrow(data), 784)
-# dim(x_test) <- c(nrow(x_test), 784)
-
-# rescale
-# x_train <- x_train / 255
-# x_test <- x_test / 255
 
 #y_train <- to_categorical(data, 10001)
 
 # Here we consider the simplest model (linear stack of layers)
 model <- keras_model_sequential() 
 model %>% 
-  layer_dense(units = 1000, activation = "relu", input_shape = c(10000)) %>% 
-  layer_dropout(rate = 0.4) %>% 
-  layer_dense(units = 500, activation = "relu") %>%
+  layer_dense(units = 625, activation = "relu", input_shape = c(10000)) %>% 
+  layer_dropout(rate = 0.4) %>%
+  layer_dense(units = 312, activation = "relu") %>%
   layer_dropout(rate = 0.4) %>%
   layer_dense(units = 3, activation = "softmax")
 
@@ -115,7 +83,7 @@ model %>% compile(
 # Training
 history <- model %>% fit(
   x_train, y_train, 
-  epochs = 30,
+  epochs = 100, batch_size = 20,
   validation_split = 0.2
 )
 
@@ -123,3 +91,112 @@ plot(history)
 
 # Evaluation of model
 model %>% evaluate(x_test, y_test,verbose = 0)
+
+## CNN model
+# Functions
+processImage = function(img) {
+  # Resize
+  output = resizeImage(img, width = 100, height = 100, method = 'bilinear')
+  # Scaling
+  output = output / max(output)
+  # Return
+  return(output)
+}
+
+# Read images
+nb_cats = 590
+nb_cars = 485
+nb_flowers = 521
+
+x_train = list()
+y_train = array()
+
+# 0 = cat
+# 1 = car
+# 2 = flower
+for (i in 1:nb_cats) {
+  filename = paste('cat_train_', i, '.jpg', sep = "")
+  path = file.path(getwd(), 'images_train', 'cat', filename)
+  img = readImage(path)
+  img = processImage(img)
+  x_train[[i]] = img
+  y_train[i] = 0
+}
+
+for (i in 1:nb_cars) {
+  filename = paste('car_train_', i, '.jpg', sep = "")
+  path = file.path(getwd(), 'images_train', 'car', filename)
+  img = readImage(path)
+  img = processImage(img)
+  x_train[[nb_cats + i]] = img
+  y_train[nb_cats + i] = 1
+}
+
+for (i in 1:nb_flowers) {
+  filename = paste('flower_train_', i, '.jpg', sep = "")
+  path = file.path(getwd(), 'images_train', 'flower', filename)
+  img = readImage(path)
+  img = processImage(img)
+  x_train[[i + nb_cars + nb_cats]] = img
+  y_train[i + nb_cars + nb_cats] = 2
+}
+
+y_train <- to_categorical(y_train, num_classes = 3)
+
+# CNN model (Convolutional NN)
+model <- keras_model_sequential() 
+model %>% 
+  
+  # First hidden CNN layer
+  layer_conv_2d(
+    filter = 32, kernel_size = c(3, 3), padding = "same",
+    input_shape = c(100, 100, 3)
+  ) %>%
+  layer_activation("relu") %>%
+  
+  # Second layer
+  layer_conv_2d(filter = 32, kernel_size = c(3, 3)) %>%
+  layer_activation("relu") %>%
+  
+  # Use max pooling
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+  layer_dropout(0.25) %>%
+  
+  # Two more hidden layers
+  layer_conv_2d(filter = 32, kernel_size = c(3, 3), padding = "same") %>%
+  layer_activation("relu") %>%
+  layer_conv_2d(filter = 32, kernel_size = c(3, 3)) %>%
+  layer_activation("relu") %>%
+  
+  # Use max pooling once more
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>%
+  layer_dropout(0.25) %>%
+  
+  # Flatten max filtered output into feature vector and feed into dense layer
+  layer_flatten() %>%
+  layer_dense(512) %>%
+  layer_activation("relu") %>%
+  layer_dropout(0.5) %>%
+  
+  # Outputs from dense layer are projected ono 3 unit output layer
+  layer_dense(3) %>%
+  layer_activation("softmax")
+
+summary(model)
+
+# Compile the model
+model %>% compile(
+  loss = "categorical_crossentropy",
+  optimizer = optimizer_rmsprop(),
+  metrics = c("accuracy")
+)
+
+# Training
+history <- model %>% fit(
+  x_train, y_train, 
+  epochs = 200, batch_size = 20,
+  validation_split = 0.2
+)
+
+plot(history)
+
